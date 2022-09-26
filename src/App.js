372300d1e265c23ec2cfe2bbff13b4978ca8f010
Eran Geva <eran.geva@gmail.com>
2022-09-26 23:25:58 +0300

    add title move zoom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap, ZoomControl } from 'react-leaflet';
 import useLocalStorage from 'use-local-storage';
 import { TipLabel } from './TipLabel.js';
 import 'leaflet/dist/leaflet.css';
@@ -16,32 +16,41 @@ const App = () => {
   const [labels, setLabels] = useLocalStorage("labels", []);
   const firstCenter = labels && labels.length ? labels[0].latlng : [32.1, 34.8];
   return (
-    <MapContainer
-      center={firstCenter}
-      zoom={14}
-      style={{ height: '100vh' }}
-      key={'map'}
-    >
-      <LocateClicks
-        createLabel={(latlng) => {
-          const newLabels = labels.map(l => {
-            const {latlng, text} = l;
-            return {latlng, text};
-          });
-          newLabels.push({ latlng, text: 'Label ' + labels.length, first: true });
-          setLabels(newLabels);
-        }}
+    <div>
+      <div className="titleText">
+        <h3>Map Labels</h3>
+        <p>Just click the map to add labels</p>
+        {/* <hr /> */}
+      </div>
+      <MapContainer
+        center={firstCenter}
+        zoom={14}
+        zoomControl={false}
+        style={{ height: '100vh' }}
+        key={'map'}
       >
-      </LocateClicks>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+        <ZoomControl position="bottomleft" />
+        <LocateClicks
+          createLabel={(latlng) => {
+            const newLabels = labels.map(l => {
+              const { latlng, text } = l;
+              return { latlng, text };
+            });
+            newLabels.push({ latlng, text: 'Label ' + labels.length, first: true });
+            setLabels(newLabels);
+          }}
+        >
+        </LocateClicks>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-      {labels.map((lb, i) =>
-        <TipLabel key={i} currLabel={lb} labels={labels} setLabels={setLabels}></TipLabel>
-      )}
-    </MapContainer>
+        {labels.map((lb, i) =>
+          <TipLabel key={i} currLabel={lb} labels={labels} setLabels={setLabels}></TipLabel>
+        )}
+      </MapContainer>
+    </div>
   );
 }
 
